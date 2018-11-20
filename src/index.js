79af5151a9a008ee93475f0d71f42d32040153d0
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-20 21:07:46 +0200

    Tested the appReducer: OK

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-console.log('Implementing actions and action creators');
-import { createPost } from './actions';
-
-console.log(createPost('Achilles','Learning Redux'));
-
+console.log('Implementing reducers');
+import { createPost, editPost, setFilter } from './actions';
+import appReducer from './reducers';
 
 
+
+let state = appReducer(undefined, 'INIT_ACTION');
+console.log('initial state',state);
+const action = createPost('Achilles', 'Learning Redux');
+const action2 = createPost('nullWatch', 'Exploring Redux');
+// const filter = setFilter('random');
+// const newState = appReducer(initialState, action);
+state = appReducer(state, action);
+state = appReducer(state, action2);
+state = appReducer(state, editPost(1,'Edited post'));
+state = appReducer(state, setFilter('hidden'));
+
+console.log('state',state);
