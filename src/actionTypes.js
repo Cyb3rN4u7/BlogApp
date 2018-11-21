0b40cfc0a166cbc0a8100abc15090387f36fda2c
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-21 13:00:17 +0200

    Organized Action Creators

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,3 +1,9 @@
+// POSTS
 export const CREATE_POST = 'CREATE_POST';
 export const EDIT_POST = 'EDIT_POST';
+export const DELETE_POST = 'DELETE_POST'
+// FILTER
 export const SET_FILTER = 'SET_FILTER';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+// USERS
+export const CREATE_USER = 'CREATE_USER';
